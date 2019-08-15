a72ae59b83f27d5bd31f43920991f695a963a3a2
dpienczyn <donia19881@wp.pl>
2019-08-15 18:26:55 +0200

    Update Palindrom.java

--- Palindrom.java
+++ Palindrom.java
@@ -4,37 +4,37 @@ import java.util.Scanner;
 
 public class Palindrom {
 
-	public static boolean palindrom(String lancuch) {
-		lancuch = lancuch.replace(" ", "");
-		lancuch = (lancuch.toLowerCase());
-		// tworzymy nowy obiekt klasy StringBuilder
-		StringBuilder tekst = new StringBuilder(lancuch);
-		System.out.println(tekst);
-		// wykorzystujemu metodÍ reverse() do odwrÛcenia ≥aÒcucha
-		
-		// oraz equals() do sprawdzenia czy oba ≥aÒcuchy sπ takie same
-		
-		return lancuch.equals(tekst.reverse().toString());
-		
-		}
-		
-		 
-		
-		public static void main(String[] args){
-		
-		Scanner sc = new Scanner(System.in);
-		
-		System.out.print ("Podaj ≥aÒcuch tekstowy: " );
-		
-		// pobieramy od uøytkownika ≥aÒcuch tekstowy
-		
-		String lancuch = sc.nextLine();
-		
-		// wyúwietlamy na ekranie informacje czy podany wyraz jest palindromem
-		
-		
-		System.out.println("£aÒcuch tekstowy: " + lancuch + (palindrom(lancuch) ? " jest" : " nie jest") + " palindromem");
-		
-		}
-		
-		}
+    public static boolean palindrom(String lancuch) {
+        lancuch = lancuch.replace(" ", "");
+        lancuch = (lancuch.toLowerCase());
+        // tworzymy nowy obiekt klasy StringBuilder
+        StringBuilder tekst = new StringBuilder(lancuch);
+        System.out.println(tekst);
+        // wykorzystujemu metodƒô reverse() do odwr√≥cenia ≈Ça≈Ñcucha
+
+        // oraz equals() do sprawdzenia czy oba ≈Ça≈Ñcuchy sƒÖ takie same
+
+        return lancuch.equals(tekst.reverse().toString());
+
+    }
+
+
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Podaj ≈Ça≈Ñcuch tekstowy: ");
+
+        // pobieramy od u≈ºytkownika ≈Ça≈Ñcuch tekstowy
+
+        String lancuch = sc.nextLine();
+
+        // wy≈õwietlamy na ekranie informacje czy podany wyraz jest palindromem
+
+
+        System.out.println("≈Åa≈Ñcuch tekstowy: " + lancuch + (palindrom(lancuch) ? " jest" : " nie jest") + " palindromem");
+
+    }
+
+}
