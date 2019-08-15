7daf90beb3492ab9638019db81ff6b6e9be281f2
dpienczyn <donia19881@wp.pl>
2019-08-15 18:26:00 +0200

    Update CiagF.java

--- CiagF.java
+++ CiagF.java
@@ -1,17 +1,17 @@
 package rozmowa;
 
 public class CiagF {
-	
-	public int ciag(int f) {
-		if(f<2) {
-			return 1;
-		}
-		return ciag(f-1)+ciag(f-2);
-	}
 
-	public static void main(String[] args) {
-		CiagF g = new CiagF();
-		int h = g.ciag(5);
-		System.out.println(h);
-	}
+    public int ciag(int f) {
+        if (f < 2) {
+            return 1;
+        }
+        return ciag(f - 1) + ciag(f - 2);
+    }
+
+    public static void main(String[] args) {
+        CiagF g = new CiagF();
+        int h = g.ciag(5);
+        System.out.println(h);
+    }
 }
